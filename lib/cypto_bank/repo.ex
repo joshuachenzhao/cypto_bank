ea834a72c207bd1cf9735469b41898f0c432f5d9
Joshua Chen Zhao <crunchysouldev@gmail.com>
2020-10-25 02:27:06 +0800

    add customerized Repo.get to handle UUID error issue

--- lib/cypto_bank/repo.ex
+++ lib/cypto_bank/repo.ex
@@ -2,4 +2,25 @@ defmodule CyptoBank.Repo do
   use Ecto.Repo,
     otp_app: :cypto_bank,
     adapter: Ecto.Adapters.Postgres
+
+  @doc """
+  to handle UUID Ecto.Query.CastError :error, return tuple
+  """
+  def fetch(query, id, opts \\ []) do
+    case get(query, id, opts) do
+      nil -> {:error, :not_found}
+      schema -> {:ok, schema}
+    end
+  end
+
+  @doc """
+  to handle UUID Ecto.Query.CastError :error
+  """
+  defoverridable get: 2, get: 3
+
+  def get(query, id, opts \\ []) do
+    super(query, id, opts)
+  rescue
+    Ecto.Query.CastError -> nil
+  end
 end
