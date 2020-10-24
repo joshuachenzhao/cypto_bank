0ecd654ee7e4aab669409cb6e58527fe3483e3b2
Joshua Chen Zhao <crunchysouldev@gmail.com>
2020-10-24 23:44:03 +0800

    update simple transfer with ledger entries

--- lib/cypto_bank/transactions.ex
+++ lib/cypto_bank/transactions.ex
@@ -35,6 +35,8 @@ defmodule CyptoBank.Transactions do
     Ledger.changeset(ledger, attrs)
   end
 
+  # TODO
+  # 1. add ledger transactions
   def transfer(amount, send_acc_id, receive_acc_id) do
     Repo.transaction(fn ->
       [acc_a, acc_b] =
@@ -42,10 +44,15 @@ defmodule CyptoBank.Transactions do
 
       if acc_a.balance < amount, do: Repo.rollback(:balance_too_low)
 
+      send_ledger_attrs = %{amount: amount, account_id: send_acc_id, type: :transfer_pay}
+      receive_ledger_attrs = %{amount: amount, account_id: send_acc_id, type: :transfer_receive}
+
       update1 = acc_a |> Account.changeset(%{balance: acc_a.balance - amount}) |> Repo.update!()
       update2 = acc_b |> Account.changeset(%{balance: acc_b.balance + amount}) |> Repo.update!()
+      update3 = create_ledger(send_ledger_attrs)
+      update4 = create_ledger(receive_ledger_attrs)
 
-      {update1, update2}
+      {update1, update2, update3, update4}
     end)
   end
 end
