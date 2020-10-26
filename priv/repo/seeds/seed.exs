5121bbe2b42d42c74a61da3980e355cc33f4b701
Joshua Chen Zhao <crunchysouldev@gmail.com>
2020-10-27 02:20:33 +0800

    fix seed unused variable warning

--- priv/repo/seeds/seed.exs
+++ priv/repo/seeds/seed.exs
@@ -4,18 +4,17 @@ defmodule CyptoBank.Seeds.Seed do
   alias CyptoBank.Adjustments
   alias CyptoBank.Adjustments.Adjustment
   alias CyptoBank.Transactions
-  alias CyptoBank.Transactions.Ledger
 
   def generate() do
     # Add more seed fns here
-    seed_admins
+    seed_admins()
     seed_clients_with_trans()
 
     IO.puts("-----> Done: Seeding Complete!")
   end
 
   def seed_admins() do
-    admin_param_lists
+    admin_param_lists()
     |> Enum.each(&Accounts.create_user/1)
   end
 
@@ -30,34 +29,34 @@ defmodule CyptoBank.Seeds.Seed do
   one adjustment for deposit, one adjustment for withdrawal
   """
   def client_pair_with_trans({user_params_1, user_params_2}) do
-    with {:ok, %{user_id: user_id_1, account_id: account_id_1}} <- create_account(user_params_1),
-         {:ok, %{user_id: user_id_2, account_id: account_id_2}} <- create_account(user_params_2),
+    with {:ok, %{user_id: _user_id_1, account_id: account_id_1}} <- create_account(user_params_1),
+         {:ok, %{user_id: _user_id_2, account_id: account_id_2}} <- create_account(user_params_2),
          {:ok, %{create_deposit_ledger_step: dep_tran_1}} <-
            Transactions.deposite(Enum.random(30..100) * 10000, account_id_1),
-         {:ok, %{create_deposit_ledger_step: dep_tran_2}} <-
+         {:ok, %{create_deposit_ledger_step: _dep_tran_2}} <-
            Transactions.deposite(Enum.random(30..100) * 10000, account_id_2),
-         {:ok, %{create_withdrawal_ledger_step: with_tran_1}} <-
+         {:ok, %{create_withdrawal_ledger_step: _with_tran_1}} <-
            Transactions.withdrawal(Enum.random(1..50) * 1000, account_id_1),
          {:ok, %{create_withdrawal_ledger_step: with_tran_2}} <-
            Transactions.withdrawal(Enum.random(1..50) * 1000, account_id_2),
          {:ok,
           %{
-            create_send_ledger_step: send_tran_1,
-            create_receive_ledger_step: receive_tran_1
+            create_send_ledger_step: _send_tran_1,
+            create_receive_ledger_step: _receive_tran_1
           }} <-
            Transactions.transfer(Enum.random(1..50) * 1000, account_id_1, account_id_2),
          {:ok,
           %{
-            create_send_ledger_step: send_tran_2,
-            create_receive_ledger_step: receive_tran_2
+            create_send_ledger_step: _send_tran_2,
+            create_receive_ledger_step: _receive_tran_2
           }} <-
            Transactions.transfer(Enum.random(1..50) * 1000, account_id_2, account_id_1),
-         {:ok, %Adjustment{} = dep_adj} <-
+         {:ok, %Adjustment{} = _dep_adj} <-
            Adjustments.create_adjustment(%{
              "original_ledger_id" => dep_tran_1.id,
              "amount" => Enum.random(1..20000) * 100
            }),
-         {:ok, %Adjustment{} = with_adj} <-
+         {:ok, %Adjustment{} = _with_adj} <-
            Adjustments.create_adjustment(%{
              "original_ledger_id" => with_tran_2.id,
              "amount" => -Enum.random(1..20000) * 100
