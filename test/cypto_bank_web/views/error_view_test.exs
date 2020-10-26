bd54bd5f11bdb30dee4696a50cd366ca8d01758c
Joshua Chen Zhao <crunchysouldev@gmail.com>
2020-10-27 00:08:01 +0800

    update error_view_test

--- test/cypto_bank_web/views/error_view_test.exs
+++ test/cypto_bank_web/views/error_view_test.exs
@@ -4,6 +4,10 @@ defmodule CyptoBankWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  test "renders 401.json" do
+    assert render(CyptoBankWeb.ErrorView, "401.json", []) == %{errors: %{detail: "Unauthorized"}}
+  end
+
   test "renders 404.json" do
     assert render(CyptoBankWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
   end
