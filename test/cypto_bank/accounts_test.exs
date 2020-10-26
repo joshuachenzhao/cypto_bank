78a603320bf16c8dfacc1ce846a43da8c770a744
Joshua Chen Zhao <crunchysouldev@gmail.com>
2020-10-26 21:19:42 +0800

    add accounts tests

--- test/cypto_bank/accounts_test.exs
+++ test/cypto_bank/accounts_test.exs
@@ -1,76 +1,97 @@
 defmodule CyptoBank.AccountsTest do
   use CyptoBank.DataCase
 
+  import CyptoBank.Factory
+
   alias CyptoBank.Accounts
+  alias CyptoBank.Accounts.{Account, User}
 
   describe "users" do
-    alias CyptoBank.Accounts.User
+    @valid_attrs %{email: "mail@email.com", password: "password"}
 
-    @valid_attrs %{email: "some email", is_admin: false, password: "some password"}
-    @update_attrs %{
-      email: "some updated email",
-      is_admin: true,
-      password: "some updated password"
-    }
-    @invalid_attrs %{email: nil, is_admin: nil, password: nil}
-
-    def user_fixture(attrs \\ %{}) do
-      {:ok, user} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Accounts.create_user()
-
-      user
-    end
-
-    def user_without_password(attrs \\ %{}) do
-      %{user_fixture(attrs) | password: nil}
+    test "get_user!/1 returns the user with given id" do
+      user = insert(:user) |> user_no_password()
+      assert Accounts.get_user!(user.id) == user
     end
 
     test "list_users/0 returns all users" do
-      user = user_without_password()
-      assert Accounts.list_users() == [user]
-    end
-
-    test "get_user!/1 returns the user with given id" do
-      user = user_without_password()
-      assert Accounts.get_user!(user.id) == user
+      users = insert_list(3, :user) |> Enum.map(&user_no_password/1)
+      assert Accounts.list_users() == users
     end
 
     test "create_user/1 with valid data creates a user" do
       assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
-      assert user.email == "some email"
+      assert user.email == "mail@email.com"
       assert user.is_admin == false
-      assert Bcrypt.verify_pass("some password", user.password_hash)
-    end
-
-    test "create_user/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
-    end
-
-    test "update_user/2 with valid data updates the user" do
-      user = user_fixture()
-      assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
-      assert user.email == "some updated email"
-      assert user.is_admin == true
-      assert Bcrypt.verify_pass("some updated password", user.password_hash)
-    end
-
-    test "update_user/2 with invalid data returns error changeset" do
-      user = user_without_password()
-      assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
-      assert user == Accounts.get_user!(user.id)
+      assert Bcrypt.verify_pass("password", user.password_hash)
     end
 
     test "authenticate_user/2 authenticates the user" do
-      user = user_without_password()
+      {:ok, user} = Accounts.create_user(%{email: "email@email.com", password: "password"})
+
+      assert {:ok, authenticated_user} = Accounts.authenticate_user("email@email.com", "password")
+      assert user |> user_no_password == authenticated_user
+    end
+
+    test "authenticate_user/2 fails with wrong email or password" do
+      user = insert(:user)
+      user_no_password = insert(:user) |> user_no_password()
 
       assert {:error, "Wrong email or password"} = Accounts.authenticate_user("wrong email", "")
 
-      assert {:ok, authenticated_user} =
-               Accounts.authenticate_user(user.email, @valid_attrs.password)
+      assert {:error, "Wrong email or password"} =
+               Accounts.authenticate_user("", "wrong password")
 
-      assert user == authenticated_user
+      assert {:error, "Wrong email or password"} = Accounts.authenticate_user("", "")
+    end
+  end
+
+  describe "accounts" do
+    test "create_account_for_user/2 creates an account for user_id" do
+      user = insert(:user)
+
+      assert {:ok, %Account{} = account} = Accounts.create_account_for_user(user.id)
+      assert account.balance == 0
+      assert account.user_id == user.id
+    end
+
+    test "list_accounts_for_user/1 list all acounts for admin" do
+      admin = insert(:admin)
+      accounts = insert_list(5, :account)
+
+      assert accounts = Accounts.list_accounts_for_user(admin)
+    end
+
+    test "list_accounts_for_user/1 list all acounts belongs to a user" do
+      user = insert(:user)
+      owned_accounts = insert_list(5, :account, user: user)
+      no_owned_accounts = insert_list(5, :account)
+
+      assert owned_accounts_accounts = Accounts.list_accounts_for_user(user)
+      assert 5 == Accounts.list_accounts_for_user(user) |> length
+    end
+
+    test "fetch_account_for_user/2 fetch an acount for admin" do
+      admin = insert(:admin)
+      user = insert(:user)
+      account = insert(:account, user: user)
+
+      assert {:ok, accounts} = Accounts.fetch_account_for_user(admin, account.id)
+    end
+
+    test "fetch_account_for_user/2 fetch an acount belongs to a user with given id" do
+      user = insert(:user)
+      account = insert(:account, user: user)
+
+      assert {:ok, accounts} = Accounts.fetch_account_for_user(user, account.id)
+    end
+
+    test "fetch_account_for_user/2 does not fetch an acount with given id that does not belong to a user" do
+      user = insert(:user)
+      another_user = insert(:user, email: "unique@email.com")
+      account = insert(:account, user: another_user)
+
+      assert {:error, :not_found} = Accounts.fetch_account_for_user(user, account.id)
     end
   end
 end
