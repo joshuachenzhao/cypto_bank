f634acf74fb4accc5dff8ca4a6752cc781405318
Joshua Chen Zhao <crunchysouldev@gmail.com>
2020-10-27 05:22:35 +0800

    add :os_mon enable os livedash

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule CyptoBank.MixProject do
   def application do
     [
       mod: {CyptoBank.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
